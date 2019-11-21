04b71ccd7db166367b1c3c081ab2cf3e43c5e968
William Lety <william.lety@gmail.com>
2019-11-21 18:21:55 +0100

    Factorisation des controllers

---
+++ Applications/Frontend/Modules/ApiController.php
@@ -0,0 +1,84 @@
+<?php
+  namespace Applications\Frontend\Modules;
+
+  class ApiController extends \Applications\Frontend\BackController
+  {
+    public function authenticated($check)
+    {
+      if (!$check) return;
+      if (!$this->isAuthorized()) {
+        header('HTTP/1.1 401 Unauthorized');
+        exit('Utilisateur non authentifié');
+      }
+    }
+
+    public function jsonArray($records)
+    {
+      $i = 0;
+      $json = null;
+      foreach ($records as $record) {
+        $js = $record->to_json();
+        if ($i !== 0) {
+            $json .= "," . $js;
+        } else {
+            $json = $js;
+        }
+        $i ++;
+      }
+      return $json;
+    }
+
+    public function beforeList(\Library\HTTPRequest $request)
+    {
+        //Test if user can list users with token
+        if ($this->method === 'GET') {
+          $this->authenticated(true);
+        }
+    }
+
+    public function executeList(\Library\HTTPRequest $request)
+    {
+        if ($this->method === 'GET') {
+            $this->getList($request);
+        } elseif ($this->method === 'POST') {
+            $this->create($request);
+        }
+    }
+
+    public function beforeBy_id(\Library\HTTPRequest $request)
+    {
+        //Test if user can get, update or delete a record with token
+        $this->authenticated(true);
+    }
+
+    public function executeBy_id(\Library\HTTPRequest $request)
+    {
+        if ($this->method === 'GET') {
+            $this->get($request);
+        } elseif ($this->method === 'PUT') {
+            $this->update($request);
+        } elseif ($this->method === 'DELETE') {
+            $this->delete($request);
+        }
+    }
+
+    public function beforeBy_username(\Library\HTTPRequest $request)
+    {
+        //Test if user can get, update or delete a record with token
+        $this->authenticated(true);
+    }
+
+    public function executeBy_username(\Library\HTTPRequest $request)
+    {
+        if ($this->method === 'GET') {
+            $this->getBy_username($request);
+        }
+        /*			else if ($this->method === 'PUT') {
+                        $this->updatesUser($request);
+                    }
+                    else if ($this->method === 'DELETE') {
+                        $this->deleteUser($request);
+                    }*/
+    }
+
+  }
