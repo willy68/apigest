153a6436a74bdb123f9d85826c53436dd6a55403
William Lety <william.lety@gmail.com>
2019-11-21 15:28:50 +0100

    formattage

--- make_models.php
+++ make_models.php
@@ -6,24 +6,24 @@ $query = "SHOW TABLES FROM {$db}";
 
 $models_dir = __DIR__.DIRECTORY_SEPARATOR.'generated_models';
 
-function getMysqlConnexion($host, $dbname, $user, $password) {
-         try{
-            	$db = new \PDO('mysql:host='.$host.';dbname='.$dbname, $user, $password);
-            	$db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
-			}
-		 catch (\PDOException $e) // On attrape les exceptions PDOException
-		    {
-        		echo 'La connexion a échoué.<br />';
-        		echo 'Informations : [', $e->getCode(), '] ', $e->getMessage(); // On affiche le n° de l'erreur ainsi que le message
-    		}	
-            $db->exec("SET NAMES 'utf8'");
+function getMysqlConnexion($host, $dbname, $user, $password)
+{
+    try {
+        $db = new \PDO('mysql:host='.$host.';dbname='.$dbname, $user, $password);
+        $db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+    } catch (\PDOException $e) { // On attrape les exceptions PDOException
+        echo 'La connexion a échoué.<br />';
+        echo 'Informations : [', $e->getCode(), '] ', $e->getMessage(); // On affiche le n° de l'erreur ainsi que le message
+    }
+    $db->exec("SET NAMES 'utf8'");
             
-            return $db;
+    return $db;
 }
 
-function getActiveRecordPHP($model_name) {
-	$model_class = ucfirst($model_name);
-	return "<?php
+function getActiveRecordPHP($model_name)
+{
+    $model_class = ucfirst($model_name);
+    return "<?php
 
 class {$model_class} extends ActiveRecord\Model {
  static \$table_name = '{$model_name}';
@@ -31,14 +31,18 @@ class {$model_class} extends ActiveRecord\Model {
 \n";
 }
 
-if ( isset( $argv ) ) {
+if (isset($argv)) {
     parse_str(
-        join( "&", array_slice( $argv, 1 )
-    ), $_GET );
+        join(
+            "&",
+            array_slice($argv, 1)
+        ),
+        $_GET
+    );
 }
 
 if (isset($_GET['models_dir'])) {
-	$models_dir = __DIR__.DIRECTORY_SEPARATOR.$_GET['models_dir'];
+    $models_dir = __DIR__.DIRECTORY_SEPARATOR.$_GET['models_dir'];
 }
 
 $dao = getMysqlConnexion($host, $db, $user, $password);
@@ -46,16 +50,15 @@ $dao = getMysqlConnexion($host, $db, $user, $password);
 $tables = $dao->query($query);
 
 if (!is_dir($models_dir)) {
-	mkdir($models_dir, 0777);
+    mkdir($models_dir, 0777);
 }
 
 while ($table = $tables->fetch()) {
-	$model_name = $table[0];
-	$model = getActiveRecordPHP($model_name);
-  if (($handle = fopen($models_dir.DIRECTORY_SEPARATOR.ucfirst($model_name).'.php', 'x'))) {
-		fwrite($handle, $model);
-		fclose($handle);
-	}
-	print($model);
+    $model_name = $table[0];
+    $model = getActiveRecordPHP($model_name);
+    if (($handle = fopen($models_dir.DIRECTORY_SEPARATOR.ucfirst($model_name).'.php', 'x'))) {
+        fwrite($handle, $model);
+        fclose($handle);
+    }
+    print($model);
 }
-
